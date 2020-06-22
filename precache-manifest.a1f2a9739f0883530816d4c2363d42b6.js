6147c01fde57ebfd0e24af390d3581abd5c822a2
Anton Kurdo <anton.kurdo@gmail.com>
2020-06-22 16:20:25 +0300

    Updates

---
+++ precache-manifest.a1f2a9739f0883530816d4c2363d42b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cca38a0c54420ea9a228f00659472a7",
+    "url": "/NASA_PIC_CALENDAR/index.html"
+  },
+  {
+    "revision": "d1108bad07901b166b14",
+    "url": "/NASA_PIC_CALENDAR/static/css/2.80468ea7.chunk.css"
+  },
+  {
+    "revision": "4e3d58f31a82e5459745",
+    "url": "/NASA_PIC_CALENDAR/static/css/main.5332b9f1.chunk.css"
+  },
+  {
+    "revision": "d1108bad07901b166b14",
+    "url": "/NASA_PIC_CALENDAR/static/js/2.59c03166.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NASA_PIC_CALENDAR/static/js/2.59c03166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e3d58f31a82e5459745",
+    "url": "/NASA_PIC_CALENDAR/static/js/main.ee03225a.chunk.js"
+  },
+  {
+    "revision": "41a4a865e49c542cc638",
+    "url": "/NASA_PIC_CALENDAR/static/js/runtime-main.59147325.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/NASA_PIC_CALENDAR/precache-manifest.a1f2a9739f0883530816d4c2363d42b6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NASA_PIC_CALENDAR/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
